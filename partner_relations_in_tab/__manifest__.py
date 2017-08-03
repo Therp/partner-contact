6d248267ebdc190e8588cfa048b17d94c17d8359
George Daramouskas <gdaramouskas@therp.nl>
2017-08-03 15:49:37 +0200

    [PORT] Ports partner_relations_in_tab to version 10.0

---
+++ partner_relations_in_tab/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2014-2017 Therp BV <http://therp.nl>.
+# License AGPL-3.0 or later <http://www.gnu.org/licenses/agpl.html>.
+{
+    "name": "Show partner relations in own tab",
+    "version": "10.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "complexity": "normal",
+    "description": """
+This module adds the possibility to show certain partner relations in its own
+tab instead of the list of all relations. This can be useful if certain
+relation types are regularly used and should be overseeable at a glace.
+    """,
+    "category": "Customer Relationship Management",
+    "depends": [
+        'partner_multi_relation',
+        'web_compute_domain_x2many',
+    ],
+    "data": [
+        "view/res_partner_relation_type.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
