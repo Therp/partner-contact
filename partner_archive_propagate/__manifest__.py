a00840fc6ab1f54373de993f595e6e862eb5a543
Nikos Tsirintanis <ntsirintanis@therp.nl>
2026-05-26 11:40:36 +0200

    fixup! [MIG] partner_archive_propagate: Migration to 18.0

--- partner_archive_propagate/__manifest__.py
+++ partner_archive_propagate/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "Therp BV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "license": "AGPL-3",
-    "depends": ["base", "mail"],
+    "depends": ["base_setup", "mail"],
     "data": [
         "security/ir.model.access.csv",
         "views/res_config_settings_views.xml",
